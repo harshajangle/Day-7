d61f5e9f60a814bf3fbc4f4ce4c47186adcc6eaf
harshajangle <ce24dse021@ybit.ac.in>
2026-04-08 16:48:36 +0530

    Add files via upload

---
+++ LinkedList.py
@@ -0,0 +1,34 @@
+# class Node:
+#     def __init__(self,data):
+#         self.data=data
+#         self.next=None
+        
+        
+# class LinkedList:
+#     def __init__(self):
+#         self.head=None
+            
+# linkedlist=LinkedList()
+# linkedlist.head=Node(5)
+# second  =Node(10)
+# third   =Node(15)
+# fourth   =Node(20)
+# #linking part
+# linkedlist.head.next=second
+# second.next=third
+# third.next=fourth
+
+# print(linkedlist.head.data)
+# print(second.data)
+# print(third.data)
+# print(fourth.data)
+        
+# print(linkedlist.head.next)
+# print(second.next)
+# print(third.next)
+# print(fourth.next)
+# #display linkedlist
+# while linkedlist.head!=None:
+#     print("|",linkedlist.head.data,"|",linkedlist.head.next,"->",end="")
+#     linkedlist.head=linkedlist.head.next
+    
